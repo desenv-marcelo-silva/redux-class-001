78b6f775e34466ed3a127fe5ccb33dd1d96120e5
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-15 21:26:53 -0300

    Geração das rotas básicas.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World Redux!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   );
 }
 
