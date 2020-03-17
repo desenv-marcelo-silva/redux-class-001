f5a545db5d52d427347b8f052ba6e19f53f41a6e
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-17 08:10:28 -0300

    Estilização da Home|Instalação polished.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,83 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return (<>
-    <h1>Home</h1>
-  </>)
+  return (<ProductList>
+    <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-adidas-energy-falcon-masculino/06/COL-6983-006/COL-6983-006_zoom1.jpg?ts=1562251104&ims=326x" alt="Tênis Adidas"/>
+      <strong>Tênis adidas legal</strong>
+      <span>R$ 329,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#FFF" /> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+    <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-adidas-energy-falcon-masculino/06/COL-6983-006/COL-6983-006_zoom1.jpg?ts=1562251104&ims=326x" alt="Tênis Adidas"/>
+      <strong>Tênis adidas legal</strong>
+      <span>R$ 329,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#FFF" /> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+    <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-adidas-energy-falcon-masculino/06/COL-6983-006/COL-6983-006_zoom1.jpg?ts=1562251104&ims=326x" alt="Tênis Adidas"/>
+      <strong>Tênis adidas legal Tênis adidas legal Tênis adidas legal Tênis adidas</strong>
+      <span>R$ 329,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#FFF" /> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+    <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-adidas-energy-falcon-masculino/06/COL-6983-006/COL-6983-006_zoom1.jpg?ts=1562251104&ims=326x" alt="Tênis Adidas"/>
+      <strong>Tênis adidas legal</strong>
+      <span>R$ 329,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#FFF" /> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+    <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-adidas-energy-falcon-masculino/06/COL-6983-006/COL-6983-006_zoom1.jpg?ts=1562251104&ims=326x" alt="Tênis Adidas"/>
+      <strong>Tênis adidas legal</strong>
+      <span>R$ 329,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#FFF" /> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+    <li>
+      <img src="https://static.netshoes.com.br/produtos/tenis-adidas-energy-falcon-masculino/06/COL-6983-006/COL-6983-006_zoom1.jpg?ts=1562251104&ims=326x" 
+           alt="Tênis Adidas"/>
+      <strong>Tênis adidas legal</strong>
+      <span>R$ 329,90</span>
+
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#FFF" /> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+  </ProductList>)
 }
