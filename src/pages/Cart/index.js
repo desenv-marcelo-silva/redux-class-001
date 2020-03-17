9c7a6572f96ee0e6c88ac4ed653534bf29a0467a
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-17 08:42:14 -0300

    Estilização do carrinho de compras.

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <>
-      <h1>Cart</h1>
-    </>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/tenis-adidas-energy-falcon-masculino/06/COL-6983-006/COL-6983-006_zoom1.jpg?ts=1562251104&ims=326x" 
+                  alt="Tênis Adidas"/>
+            </td>
+            <td>
+              <strong>Tênis beeem loucooo</strong>
+              <span>R$ 329,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 659,33</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1229,20</strong>
+        </Total>
+      </footer>
+    </Container>
   )
 }
