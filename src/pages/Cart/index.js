5a8e36615afd93676899c927880516c7dbbb529b
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-19 08:25:46 -0300

    Usando bindActionCreators: dispatch direto para as props.

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import {
   MdRemoveCircleOutline,
@@ -9,7 +10,9 @@ import {
 
 import {Container, ProductTable, Total} from './styles';
 
-function Cart({cart, dispatch}) {
+import * as CartActions from '../../store/modules/cart/actions';
+
+function Cart({cart, removeFromCart}) {
   return (
     <Container>
       <ProductTable>
@@ -47,11 +50,7 @@ function Cart({cart, dispatch}) {
                 <strong>R$ 659,33</strong>
               </td>
               <td>
-                <button
-                  type="button"
-                  onClick={() =>
-                    dispatch ({type: 'REMOVE_FROM_CART', id: product.id})}
-                >
+                <button type="button" onClick={ () => removeFromCart(product.id) }>
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
@@ -75,5 +74,7 @@ function Cart({cart, dispatch}) {
 const mapStateToProps = state => ({
   cart: state.cart,
 });
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
 
-export default connect (mapStateToProps) (Cart);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,66 +1,70 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import api from '../../services/api';
+import * as CartActions from '../../store/modules/cart/actions';
 
-import { formatPrice } from '../../util/format';
+import {formatPrice} from '../../util/format';
 
-import { ProductList } from './styles';
+import {ProductList} from './styles';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super (props);
     this.state = {
       products: [],
     };
   }
 
-  async componentDidMount() {
-    const response = await api.get('products');
+  async componentDidMount () {
+    const response = await api.get ('products');
 
-    const data = response.data.map( product => (
-      {
-        ...product,
-        priceFormatted: formatPrice(product.price),
-      }
-    ))
-    this.setState({ products: data });
-  } 
-
-  handleAddProduct = product => {
-      //dispatch veio por conta do connect no export
-      const { dispatch } = this.props;
-
-      dispatch({
-        type: 'ADD_TO_CART',
-        product,
-      })
+    const data = response.data.map (product => ({
+      ...product,
+      priceFormatted: formatPrice (product.price),
+    }));
+    this.setState ({products: data});
   }
 
-  render() {
-    const { products } = this.state;
+  handleAddProduct = product => {
+    const { addToCart } = this.props;
 
-    return (<ProductList>
-      { products.map( product => (
+    addToCart(product);
+  };
+
+  render() {
+    const {products} = this.state;
+
+    return (
+      <ProductList>
+        {products.map (product => (
           <li key={product.id}>
-            <img src={product.image} alt={product.title}/>
+            <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
-            <button type="button" onClick={() => this.handleAddProduct(product)}>
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct (product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" /> 3
               </div>
 
               <span>ADICIONAR AO CARRINHO</span>
             </button>
-            </li>
-      ))}
-    </ProductList>);    
+          </li>
+        ))}
+      </ProductList>
+    );
   }
 }
 
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
 //connect faz a ligação do component com a store do redux
 // aceita uma função dentro do connect
-export default connect()(Home);
+export default connect(null, mapDispatchToProps)(Home);
