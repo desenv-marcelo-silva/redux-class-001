fe3d0ca7c00c76d5d5caec8e2985f05cb689ea50
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-04-01 12:48:58 -0300

    Saga do cart: variável sem utilização.

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -9,19 +9,19 @@ import { formatPrice } from '../../../util/format';
 import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
-  const productExists = yield select(
-    state => state.cart.find( p => p.id === id),
-  )
+  const productExists = yield select((state) =>
+    state.cart.find((p) => p.id === id)
+  );
 
   const stock = yield call(api.get, `/stock/${id}`);
 
   const stockAmount = stock.data.amount;
   const currentAmount = productExists ? productExists.amount : 0;
-  
+
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-    toast.error("Quantidade solicitada fora do estoque.");
+    toast.error('Quantidade solicitada fora do estoque.');
     return;
   }
   if (productExists) {
@@ -32,25 +32,22 @@ function* addToCart({ id }) {
       ...response.data,
       amount: 1,
       priceFormatted: formatPrice(response.data.price),
-    }
-  
+    };
+
     yield put(addToCartSuccess(data));
     history.push('/cart');
   }
 }
 
-function* updateAmount({ id, amount}) {
-  if (amount <= 0) return
-  const product = yield select(state =>
-    state.cart.find(p => p.id === id)
-    );
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
 
   const stock = yield call(api.get, `stock/${id}`);
 
   const stockAmount = stock.data.amount;
 
   if (amount > stockAmount) {
-    toast.error("Quantidade solicitada fora do estoque.");
+    toast.error('Quantidade solicitada fora do estoque.');
     return;
   }
 
@@ -59,5 +56,5 @@ function* updateAmount({ id, amount}) {
 
 export default all([
   takeLatest('@cart/ADD_REQUEST', addToCart),
-  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount)
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
 ]);
